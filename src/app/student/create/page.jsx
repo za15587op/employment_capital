ae9bcc4480f06c5fb3c29a8d318e5e7f71449a31
za15587op <za15587op@gmail.com>
2024-09-30 22:38:21 +0700

    update

--- src/app/student/create/page.jsx
+++ src/app/student/create/page.jsx
@@ -155,15 +155,22 @@ function StudentForm({ params }) {
   
         if (res.ok) {
           const data = await res.json();
+          console.log(data,"data");
           
-          // อัปเดต session ด้วย student_id ที่ได้รับ
-          await fetch("/api/update-session", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ student_id: data.student_id }),
+          
+          // // อัปเดต session ด้วย student_id ที่ได้รับ
+          // await fetch("/api/update-session", {
+          //   method: "POST",
+          //   headers: { "Content-Type": "application/json" },
+          //   body: JSON.stringify({ student_id: data.student_id }),
             
-          });
-          console.log(body,"body");
+          // });
+          // console.log(body,"body");
+
+           // เรียก setSession เพื่ออัปเดต session ฝั่ง client (ใช้กับ next-auth)
+        const { setSession } = require('next-auth/react');
+        setSession({ ...session, user: { ...session.user, student_id: data.student_id } });
+
           
           setError("");
           setSuccess(true); // Trigger success notification
