8c47be66c34b94084a08805f80a80c6adeb12e32
za15587op <za15587op@gmail.com>
2024-10-01 21:40:42 +0700

    ีupdate

--- src/app/api/student_scholarships/route.js
+++ src/app/api/student_scholarships/route.js
@@ -1,18 +1,44 @@
 import { NextResponse } from "next/server";
+import path from "path";
+import fs from "fs";
 import ScholarshipRegistrations from "../../../../models/scholarshipregistrations";
 import DateTimeAvailable from "../../../../models/datetimeavailable";
-const { uploadFileToHostinger } = require('../../../../lib/fileUploadHelper');
 
+// กำหนดโฟลเดอร์ที่จะเก็บไฟล์ที่อัปโหลด
+const UPLOAD_DIR = path.resolve(process.cwd(), "public/uploads");
 
+// export const config = {
+//   api: {
+//     bodyParser: false, // ปิดการแยกข้อมูลของ Next.js เพื่อจัดการข้อมูล multipart form data
+//   },
+// };
+
+/// ฟังก์ชันช่วยจัดการอัปโหลดไฟล์และเปลี่ยนชื่อไฟล์
 async function handleFileUpload(formData) {
   const scholarships = JSON.parse(formData.get("scholarships"));
+
   const file = formData.get("file");
   const student_id = formData.get("student_id");
   const scholarship_id = formData.get("scholarship_id");
 
   if (file) {
-    const fileUrl = await uploadFileToHostinger(file, student_id, scholarship_id, scholarships);
-    return fileUrl; // ส่งคืน URL ของไฟล์ที่ถูกอัปโหลด
+    const buffer = Buffer.from(await file.arrayBuffer());
+
+    // ตรวจสอบว่ามีโฟลเดอร์อัปโหลดแล้วหรือไม่ ถ้าไม่มีให้สร้างขึ้นมา
+    if (!fs.existsSync(UPLOAD_DIR)) {
+      fs.mkdirSync(UPLOAD_DIR, { recursive: true });
+    }
+
+    // สร้างชื่อไฟล์ใหม่โดยใช้รหัสนิสิต, รหัสทุน, ปีการศึกษา และเทอม
+    const extension = path.extname(file.name); // นามสกุลไฟล์
+    const newFileName = `${student_id}_${scholarship_id}_${scholarships.academic_year}_${scholarships.academic_term}${extension}`;
+    const filePath = path.join(UPLOAD_DIR, newFileName);
+
+    // บันทึกไฟล์ไปยังโฟลเดอร์ที่กำหนด
+    fs.writeFileSync(filePath, buffer);
+
+    // ส่งคืนพาธไฟล์เพื่อนำไปเก็บในฐานข้อมูล
+    return `/uploads/${newFileName}`;
   }
 
   return null; // กรณีที่ไม่มีการอัปโหลดไฟล์
@@ -48,29 +74,39 @@ export async function POST(req) {
       );
     }
 
-    // จัดการการอัปโหลดไฟล์และได้ URL ของไฟล์
-    const fileUrl = await handleFileUpload(formData);
+    // จัดการการอัปโหลดไฟล์และได้พาธไฟล์
+    const filePath = await handleFileUpload(formData);
 
     const is_parttime = formData.get("is_parttime");
     const date_available = JSON.parse(formData.get("date_available"));
-
-    // ตรวจสอบว่ามีการอัปโหลดไฟล์หรือไม่ ถ้ามี ให้ใช้ URL ของไฟล์ที่อัปโหลดจริง ๆ
-    const fileOrWorksUrl = fileUrl || related_works;
+    
+    // ตรวจสอบว่ามีการอัปโหลดไฟล์หรือไม่ ถ้ามี ให้ใช้พาธไฟล์ที่อัปโหลดจริง ๆ
+    const fileOrWorksPath = filePath || related_works;
 
     // เรียกใช้การสร้าง record ในฐานข้อมูล
-    const registrationId = await ScholarshipRegistrations.create({
+    const registrationId = await ScholarshipRegistrations.create(
       student_id,
       scholarship_id,
-      file_path: fileOrWorksUrl, // ใช้ URL ของไฟล์แทนที่ถ้ามีการอัปโหลดไฟล์
-      student_status,
-    });
+      fileOrWorksPath, // ใช้พาธไฟล์แทนที่ถ้ามีการอัปโหลดไฟล์
+      student_status
+    );
 
-    // บันทึกข้อมูลวันเวลาที่สามารถทำงานในฐานข้อมูล
-    await DateTimeAvailable.create({
-      registrationId,
-      date_available: date_available,
-      is_parttime: is_parttime,
-    });
+
+// เรียกใช้การสร้าง record ในฐานข้อมูล
+await DateTimeAvailable.create(
+  registrationId,
+  date_available, // ส่งเป็น JSON string ไปบันทึกในฐานข้อมูล
+  is_parttime
+);
+
+    // // บันทึกข้อมูลวันเวลาที่สามารถทำงานในฐานข้อมูล
+    // for (const day of date_available) {
+    //   await DateTimeAvailable.create(
+    //     registrationId,
+    //     day,
+    //     is_parttime,
+    //   );
+    // }
 
     return NextResponse.json({
       success: true,
@@ -85,6 +121,7 @@ export async function POST(req) {
   }
 }
 
+
 export async function GET(req) {
   const { searchParams } = new URL(req.url);
   const student_id = searchParams.get('student_id');
@@ -92,7 +129,8 @@ export async function GET(req) {
 
   try {
     const existingRegistration = await ScholarshipRegistrations.findOne({
-      where: { student_id, scholarship_id },
+      student_id,
+      scholarship_id,
     });
 
     if (existingRegistration) {
@@ -106,6 +144,7 @@ export async function GET(req) {
   }
 }
 
+
 export async function DELETE(req) {
   try {
     const url = new URL(req.url);
@@ -117,8 +156,9 @@ export async function DELETE(req) {
         { status: 400 }
       );
     }
-    await DateTimeAvailable.delete({ where: { registrationId: regist_id } });
-    await ScholarshipRegistrations.delete({ where: { id: regist_id } });
+    await DateTimeAvailable.delete(regist_id);
+
+    await ScholarshipRegistrations.delete(regist_id); 
 
     return NextResponse.json(
       { message: "ScholarshipRegistrations deleted successfully." },
@@ -132,181 +172,4 @@ export async function DELETE(req) {
     );
   }
 }
-
-
-
-// import { NextResponse } from "next/server";
-// import path from "path";
-// import fs from "fs";
-// import ScholarshipRegistrations from "../../../../models/scholarshipregistrations";
-// import DateTimeAvailable from "../../../../models/datetimeavailable";
-
-// // กำหนดโฟลเดอร์ที่จะเก็บไฟล์ที่อัปโหลด
-// const UPLOAD_DIR = path.resolve(process.cwd(), "https://employment-capital-vercel-app.preview-domain.com/public/uploads");
-
-// // export const config = {
-// //   api: {
-// //     bodyParser: false, // ปิดการแยกข้อมูลของ Next.js เพื่อจัดการข้อมูล multipart form data
-// //   },
-// // };
-
-// /// ฟังก์ชันช่วยจัดการอัปโหลดไฟล์และเปลี่ยนชื่อไฟล์
-// async function handleFileUpload(formData) {
-//   const scholarships = JSON.parse(formData.get("scholarships"));
-
-//   const file = formData.get("file");
-//   const student_id = formData.get("student_id");
-//   const scholarship_id = formData.get("scholarship_id");
-
-//   if (file) {
-//     const buffer = Buffer.from(await file.arrayBuffer());
-
-//     // ตรวจสอบว่ามีโฟลเดอร์อัปโหลดแล้วหรือไม่ ถ้าไม่มีให้สร้างขึ้นมา
-//     if (!fs.existsSync(UPLOAD_DIR)) {
-//       fs.mkdirSync(UPLOAD_DIR, { recursive: true });
-//     }
-
-//     // สร้างชื่อไฟล์ใหม่โดยใช้รหัสนิสิต, รหัสทุน, ปีการศึกษา และเทอม
-//     const extension = path.extname(file.name); // นามสกุลไฟล์
-//     const newFileName = `${student_id}_${scholarship_id}_${scholarships.academic_year}_${scholarships.academic_term}${extension}`;
-//     const filePath = path.join(UPLOAD_DIR, newFileName);
-
-//     // บันทึกไฟล์ไปยังโฟลเดอร์ที่กำหนด
-//     fs.writeFileSync(filePath, buffer);
-
-//     // ส่งคืนพาธไฟล์เพื่อนำไปเก็บในฐานข้อมูล
-//     return `https://employment-capital-vercel-app.preview-domain.com/uploads/${newFileName}`;
-//   }
-
-//   return null; // กรณีที่ไม่มีการอัปโหลดไฟล์
-// }
-
-// export async function POST(req) {
-//   try {
-//     // แยกข้อมูลฟอร์มจากร่างกายคำขอ
-//     const formData = await req.formData();
-
-//     const student_id = formData.get("student_id");
-//     const scholarship_id = formData.get("scholarship_id");
-//     const related_works = formData.get("related_works");
-//     const student_status = "Pending";
-
-//     // ตรวจสอบว่า student_id และ scholarship_id มีหรือไม่
-//     if (!student_id || !scholarship_id) {
-//       return NextResponse.json(
-//         { success: false, message: "Student ID หรือ Scholarship ID หายไป." },
-//         { status: 400 }
-//       );
-//     }
-
-//     // ตรวจสอบว่าผู้ใช้ได้สมัครทุนนี้ไปแล้วหรือยัง
-//     const existingRegistration = await ScholarshipRegistrations.findOne({
-//       where: { student_id, scholarship_id },
-//     });
-
-//     if (existingRegistration) {
-//       return NextResponse.json(
-//         { success: false, message: "คุณได้สมัครทุนนี้ไปแล้ว." },
-//         { status: 400 }
-//       );
-//     }
-
-//     // จัดการการอัปโหลดไฟล์และได้พาธไฟล์
-//     const filePath = await handleFileUpload(formData);
-
-//     const is_parttime = formData.get("is_parttime");
-//     const date_available = JSON.parse(formData.get("date_available"));
-    
-//     // ตรวจสอบว่ามีการอัปโหลดไฟล์หรือไม่ ถ้ามี ให้ใช้พาธไฟล์ที่อัปโหลดจริง ๆ
-//     const fileOrWorksPath = filePath || related_works;
-
-//     // เรียกใช้การสร้าง record ในฐานข้อมูล
-//     const registrationId = await ScholarshipRegistrations.create(
-//       student_id,
-//       scholarship_id,
-//       fileOrWorksPath, // ใช้พาธไฟล์แทนที่ถ้ามีการอัปโหลดไฟล์
-//       student_status
-//     );
-
-
-// // เรียกใช้การสร้าง record ในฐานข้อมูล
-// await DateTimeAvailable.create(
-//   registrationId,
-//   date_available, // ส่งเป็น JSON string ไปบันทึกในฐานข้อมูล
-//   is_parttime
-// );
-
-//     // // บันทึกข้อมูลวันเวลาที่สามารถทำงานในฐานข้อมูล
-//     // for (const day of date_available) {
-//     //   await DateTimeAvailable.create(
-//     //     registrationId,
-//     //     day,
-//     //     is_parttime,
-//     //   );
-//     // }
-
-//     return NextResponse.json({
-//       success: true,
-//       message: "การลงทะเบียนทุนการศึกษาสำเร็จ.",
-//     });
-//   } catch (error) {
-//     console.error("เกิดข้อผิดพลาดระหว่างการลงทะเบียนทุน:", error);
-//     return NextResponse.json(
-//       { success: false, message: "เกิดข้อผิดพลาดระหว่างการลงทะเบียน.", error: error.message },
-//       { status: 500 }
-//     );
-//   }
-// }
-
-
-// export async function GET(req) {
-//   const { searchParams } = new URL(req.url);
-//   const student_id = searchParams.get('student_id');
-//   const scholarship_id = searchParams.get('scholarship_id');
-
-//   try {
-//     const existingRegistration = await ScholarshipRegistrations.findOne({
-//       student_id,
-//       scholarship_id,
-//     });
-
-//     if (existingRegistration) {
-//       return NextResponse.json({ exists: true });
-//     } else {
-//       return NextResponse.json({ exists: false });
-//     }
-//   } catch (error) {
-//     console.error("Error checking registration:", error);
-//     return NextResponse.json({ success: false, message: 'เกิดข้อผิดพลาดขณะตรวจสอบการสมัคร' }, { status: 500 });
-//   }
-// }
-
-
-// export async function DELETE(req) {
-//   try {
-//     const url = new URL(req.url);
-//     const regist_id = url.searchParams.get('regist_id'); 
-
-//     if (!regist_id) {
-//       return NextResponse.json(
-//         { message: "ID is required." },
-//         { status: 400 }
-//       );
-//     }
-//     await DateTimeAvailable.delete(regist_id);
-
-//     await ScholarshipRegistrations.delete(regist_id); 
-
-//     return NextResponse.json(
-//       { message: "ScholarshipRegistrations deleted successfully." },
-//       { status: 200 }
-//     );
-//   } catch (error) {
-//     console.error("Error deleting ScholarshipRegistrations:", error);
-//     return NextResponse.json(
-//       { message: "An error occurred during ScholarshipRegistrations deletion." },
-//       { status: 500 }
-//     );
-//   }
-// }
 
--- src/app/welcome/student_scholarships/[id]/page.jsx
+++ src/app/welcome/student_scholarships/[id]/page.jsx
@@ -14,11 +14,11 @@ export default function ScholarshipRegistration({ params }) {
   const router = useRouter();
   const student_id = session?.user?.student_id || null;
 
-  const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION,
-  });
+  // const s3 = new AWS.S3({
+  //   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  //   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  //   region: process.env.AWS_REGION,
+  // });
 
   useEffect(() => {
     if (status === "loading") return;
